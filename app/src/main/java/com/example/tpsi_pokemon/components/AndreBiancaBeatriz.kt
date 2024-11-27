af2635a50e7f8620457fe774b3a58778c863ae57
pdcm11 <153228567+Andre-Branco1904@users.noreply.github.com>
2024-11-27 22:26:39 +0000

    Criação da Demo

---
+++ app/src/main/java/com/example/tpsi_pokemon/components/AndreBiancaBeatriz.kt
@@ -0,0 +1,17 @@
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tpsi_pokemon.Greeting
+import com.example.tpsi_pokemon.ui.theme.TPSI_PokemonTheme
+
+@Composable
+@Preview
+fun AndreBiancaBeatriz(modifier: Modifier = Modifier) {
+    Text(
+        text = "Olá eu sou o Bianca, o André e a Beatriza",
+        modifier = modifier
+    )
+}
